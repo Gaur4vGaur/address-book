f665453155446b32df46fbb56ed115c717909221
gaur4vgaur <vaibgaurav@gmail.com>
2018-06-15 06:55:46 +0100

    count number of males in address book

--- src/main/java/com/addressbook/AddressBook.java
+++ src/main/java/com/addressbook/AddressBook.java
@@ -1,4 +1,26 @@
 package com.addressbook;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class AddressBook {
+
+    private String fileLocaltion = "AddressBook";
+
+    public int countMales() {
+        int count = 0;
+        try (Stream<String> stream = Files.lines(Paths.get(fileLocaltion))) {
+            count = stream.filter(input -> input.contains("Male")).
+                    map(i -> 1).
+                    collect(Collectors.toList()).size();
+        } catch (IOException e) {
+            System.out.println("Error fetching file");
+            e.printStackTrace();
+        }
+
+        return count;
+    }
 }
--- src/test/java/com/addressbook/AddressBookTest.java
+++ src/test/java/com/addressbook/AddressBookTest.java
@@ -1,4 +1,16 @@
 package com.addressbook;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class AddressBookTest {
+
+    private AddressBook addressBook;
+
+    @Test
+    public void countMaleTest() {
+        addressBook = new AddressBook();
+        assertEquals("count of males must be 3", 3, addressBook.countMales());
+    }
 }
